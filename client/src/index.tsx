907575acee9ba41803cdac968f9aed669f623c28
J185_정물결 <mulgyeoljung@gmail.com>
2021-11-02 21:59:25 +0900

    💄 styled-component global-style 방식 사용
    font-face, body 스타일 지정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './assets/styles/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
