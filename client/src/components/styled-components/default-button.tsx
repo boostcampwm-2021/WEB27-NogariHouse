d562d20ff64483ded8ec5756b8c5f1a19b11c652
J185_정물결 <mulgyeoljung@gmail.com>
2021-11-02 21:57:31 +0900

    💄 Default Button 컴포넌트 구현
    styled component 방식으로 재구현

--- client/src/components/styled-components/default-button.tsx
+++ client/src/components/styled-components/default-button.tsx
@@ -1,33 +1,57 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Button from '@mui/material/Button';
-import { experimentalStyled as styled } from '@mui/material/styles';
+import styled from 'styled-components';
 
 interface DefaultButtonProps {
-    customColor : string,
-    text : string,
-    size : 'small' | 'medium' | 'large'
+  buttonType: 'primary' | 'secondary'| 'active' | 'thirdly',
+  size: 'small' | 'medium' | 'large',
+  children?: string,
+  isDisabled?: boolean
 }
 
 const sizes = {
-  small: { width: 224, height: 56 },
-  medium: { width: 224, height: 56 },
-  large: { width: 224, height: 56 },
+  small: { width: 120, height: 38, fontSize: 16 },
+  medium: { width: 224, height: 56, fontSize: 24 },
+  large: { width: 350, height: 85, fontSize: 32 },
 };
 
-const CustomDefaultButton = styled(Button)`
-    background-color : ${(props : DefaultButtonProps) => props.customColor};
-    width : ${(props : DefaultButtonProps) => sizes[props.size].width}px;
-    height: ${(props : DefaultButtonProps) => sizes[props.size].height}px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 20px;
-    font-family: "Bangers";
+const buttonTypes = {
+  primary: {
+    color: '#586A9A',
+    fontColor: '#FFFFFF',
+  },
+  secondary: {
+    color: '#4A6970',
+    fontColor: '#FFFFFF',
+  },
+  active: {
+    color: '#58964F',
+    fontColor: '#FFFFFF',
+  },
+  thirdly: {
+    color: '#9AACA1',
+    fontColor: '#FFFFFF',
+  },
+};
+
+const CustomDefaultButton = styled.button`
+     color : ${(props : DefaultButtonProps) => buttonTypes[props.buttonType].fontColor};
+     background-color : ${(props : DefaultButtonProps) => buttonTypes[props.buttonType].color};
+     width : ${(props : DefaultButtonProps) => sizes[props.size].width}px;
+     height: ${(props : DefaultButtonProps) => sizes[props.size].height}px;
+     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+     border: none;
+     border-radius: 20px;
+     font-family: "Bangers";
+     font-size: ${(props : DefaultButtonProps) => sizes[props.size].fontSize}px;
+     opacity: ${(props) => (props.disabled ? 0.5 : 1)}
 `;
 
-function DefaultButton({ customColor, text, size } : DefaultButtonProps) {
+function DefaultButton({
+  buttonType, children, size, isDisabled,
+} : DefaultButtonProps) {
   return (
-    <button type="button">
-      {text}
-    </button>
+    <CustomDefaultButton type="button" buttonType={buttonType} size={size} disabled={isDisabled}>
+      {children}
+    </CustomDefaultButton>
   );
 }
 
