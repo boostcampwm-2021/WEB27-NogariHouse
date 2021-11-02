460e43643b6cee8549f5fadbd678cac3e62c36a0
J185_정물결 <mulgyeoljung@gmail.com>
2021-11-02 22:07:31 +0900

    💄 로그인 전 메인페이지 UI 구현
    로고, 회원가입/로그인 버튼

--- client/src/views/main-view.tsx
+++ client/src/views/main-view.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
 import DefaultButton from '@styled-components/default-button';
-import MainRouter from '../routes/main';
+import MainRouter from '@routes/main';
+import LargeLogo from '@src/components/large-logo';
+
+const MainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const ButtonLayout = styled.div`
+  margin-top: 150px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 function MainView() {
+  const [isLoggedIn] = useState(false);
+  if (isLoggedIn) {
+    return (
+      <>
+        <div>side bar이다</div>
+        <MainRouter />
+        <div>side bar2이다</div>
+      </>
+    );
+  }
   return (
     <>
-      <div>side bar이다</div>
-      <DefaultButton customColor="#4A6970" size="large" text="test" />
-      <MainRouter />
-      <div>side bar2이다</div>
+      <MainLayout>
+        <LargeLogo />
+        <ButtonLayout>
+          <Link to="/signup">
+            <DefaultButton buttonType="secondary" size="large">
+              SIGN UP
+            </DefaultButton>
+          </Link>
+          <Link to="/signin">
+            <DefaultButton buttonType="thirdly" size="large">
+              SIGN IN
+            </DefaultButton>
+          </Link>
+        </ButtonLayout>
+      </MainLayout>
     </>
   );
 }
