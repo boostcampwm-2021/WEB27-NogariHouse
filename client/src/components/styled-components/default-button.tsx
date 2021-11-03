871b89b91254b6b7f2b13404e95a58ab208cff06
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-11-03 14:10:46 +0900

    🔧  tsconfig.json 파일 수정

--- client/src/components/styled-components/default-button.tsx
+++ client/src/components/styled-components/default-button.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
 
 interface DefaultButtonProps {
-  buttonType: 'primary' | 'secondary'| 'active' | 'thirdly',
-  size: 'small' | 'medium' | 'large',
-  children?: string,
-  isDisabled?: boolean
+  buttonType: 'primary' | 'secondary' | 'active' | 'thirdly';
+  size: 'small' | 'medium' | 'large';
+  children?: string;
+  isDisabled?: boolean;
 }
 
 const sizes = {
@@ -33,23 +34,31 @@ const buttonTypes = {
 };
 
 const CustomDefaultButton = styled.button`
-     color : ${(props : DefaultButtonProps) => buttonTypes[props.buttonType].fontColor};
-     background-color : ${(props : DefaultButtonProps) => buttonTypes[props.buttonType].color};
-     width : ${(props : DefaultButtonProps) => sizes[props.size].width}px;
-     height: ${(props : DefaultButtonProps) => sizes[props.size].height}px;
-     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-     border: none;
-     border-radius: 20px;
-     font-family: "Bangers";
-     font-size: ${(props : DefaultButtonProps) => sizes[props.size].fontSize}px;
-     opacity: ${(props) => (props.disabled ? 0.5 : 1)}
+  color: ${(props: DefaultButtonProps) => buttonTypes[props.buttonType].fontColor};
+  background-color: ${(props: DefaultButtonProps) => buttonTypes[props.buttonType].color};
+  width: ${(props: DefaultButtonProps) => sizes[props.size].width}px;
+  height: ${(props: DefaultButtonProps) => sizes[props.size].height}px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 20px;
+  font-family: "Bangers";
+  font-size: ${(props: DefaultButtonProps) => sizes[props.size].fontSize}px;
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
 `;
 
 function DefaultButton({
-  buttonType, children, size, isDisabled,
-} : DefaultButtonProps) {
+  buttonType,
+  children,
+  size,
+  isDisabled,
+}: DefaultButtonProps) {
   return (
-    <CustomDefaultButton type="button" buttonType={buttonType} size={size} disabled={isDisabled}>
+    <CustomDefaultButton
+      type="button"
+      buttonType={buttonType}
+      size={size}
+      disabled={isDisabled}
+    >
       {children}
     </CustomDefaultButton>
   );
