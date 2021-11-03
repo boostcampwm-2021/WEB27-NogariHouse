b8afd43cf2847652cb664b863ca2d8c18c526d43
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-11-03 22:34:21 +0900

    ✨  create room 모달 생성

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from '@styles/global-styles';
+import { RecoilRoot } from 'recoil';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
