23372f8d2b1e5dbfce272b84426c91651ed5396e
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-11-04 15:50:30 +0900

    ✨  room 생성 api 추가 (server)

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import main from './routes/main';
-
+import room from './routes/room';
 // guaranteed to get dependencies
 export default () => {
   const app = Router();
   main(app);
+  room(app);
   return app;
 };
