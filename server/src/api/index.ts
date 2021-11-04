e42893418c158a3999c43be97ddb5bf84cea10b4
정물결 <mulgyeoljung@gmail.com>
2021-11-04 18:36:02 +0900

    ✨ 이벤트 등록 요청 받기(server)

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-
-import room from '@routes/room';
 import main from '@routes/main';
+import room from '@routes/room';
 import event from '@routes/event';
 
+// guaranteed to get dependencies
 export default () => {
   const app = Router();
   main(app);
