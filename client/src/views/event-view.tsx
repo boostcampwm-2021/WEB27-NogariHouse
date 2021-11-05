aa457e5aa8ff8e43b323369ad5168506302f2460
iHoHyeon <hyunee169@naver.com>
2021-11-05 20:14:30 +0900

    🚚 util 함수 분리 및 주석 추가

--- client/src/views/event-view.tsx
+++ client/src/views/event-view.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import EventCard from '@styled-components/event-card';
 import EventRegisterModal from '@components/event-register-modal';
 import useFetchItems from '@src/hooks/useFetchItems';
+import { makeDateToHourMinute } from '@src/utils';
 
 interface EventUser {
   userId: string,
@@ -17,8 +18,6 @@ interface EventCardProps {
   users: EventUser[],
   description: string,
 }
-
-const makeDateToHourMinute = (date : Date) => `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 
 const makeEventToCard = (event: EventCardProps) => (
   <EventCard key={event.key} time={makeDateToHourMinute(new Date(event.time))} title={event.title} users={event.users} description={event.description} />
