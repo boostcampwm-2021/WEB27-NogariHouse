bddb0ed64ed7912fbd13b4d008bfc9345290b0e9
ChanHui <kinm1596@daum.net>
2021-11-07 19:30:46 +0900

    ✨  Rooms DB model 생성

---
+++ server/src/models/rooms.ts
@@ -0,0 +1,29 @@
+import { Schema, Document, model } from 'mongoose';
+
+export interface IRooms extends Document{
+  title: string,
+  type: string,
+  isAnonymous: boolean,
+  userList: Array<String>,
+}
+
+const roomSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  isAnonymous: {
+    type: Boolean,
+    required: true,
+  },
+  userList: {
+    type: [String],
+    default: [],
+  },
+});
+
+export default model<IRooms>('events', roomSchema);
