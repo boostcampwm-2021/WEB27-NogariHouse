96b0609547d818f07e07edac706cc85042c990e3
ChanHui <kinm1596@daum.net>
2021-11-08 19:29:38 +0900

    ✨  socket 연결시 필요한 로직 구현
    rooms-service에서 유저 추가하는 함수 구현
    addParticipant 함수 구현 하면서 공통되는 코드 다른 함수로 분리
    socket alias 설정

--- server/src/services/rooms-service.ts
+++ server/src/services/rooms-service.ts
@@ -3,22 +3,40 @@ import Rooms from '@models/rooms';
 import Users from '@models/users';
 import redis from 'redis';
 
-export default {
-  setRoom: async (title: string, type: string, userId: string, isAnonymous: boolean) => {
-    const participants = [userId];
-    // userId 현재 아이디 가져와야 함
-    const profileUrlObject : any = await Users.findOne({ userId: 'dlatqdlatq' }).select('profileUrl');
-    const { profileUrl } = profileUrlObject.toObject();
+const insertUser = async (userId: string, roomId: string) => {
+  // userId 현재 아이디 가져와야 함
+  const profileUrlObject : any = await Users.findOne({ userId: 'dlatqdlatq' }).select('profileUrl');
+  const { profileUrl } = profileUrlObject.toObject();
 
-    const newRoom = new Rooms({
-      title, type, isAnonymous, participants,
-    });
-    const redisClient = redis.createClient();
+  const redisClient = redis.createClient();
 
-    redisClient.hset(userId, 'roomId', `${newRoom._id}`);
-    redisClient.hset(userId, 'profileUrl', `${profileUrl}`);
-    redisClient.hset(userId, 'mic', 'true');
+  redisClient.hset(userId, 'roomId', `${roomId}`);
+  redisClient.hset(userId, 'profileUrl', `${profileUrl}`);
+  redisClient.hset(userId, 'mic', 'true');
 
-    return newRoom.save();
-  },
+  redisClient.quit();
 };
+
+let instance: any = null;
+class RoomService {
+  constructor() {
+    if (instance) return instance;
+    instance = this;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async addParticipant(roomId: string, userId: string) {
+    await Rooms.findOneAndUpdate({ _id: roomId }, { $push: { participants: userId } });
+    insertUser(userId, roomId);
+  }
+
+  async setRoom(title: string, type: string, userId: string, isAnonymous: boolean) {
+    const newRoom = new Rooms({
+      title, type, isAnonymous,
+    });
+    await newRoom.save();
+    this.addParticipant(newRoom._id, userId);
+  }
+}
+
+export = new RoomService();
