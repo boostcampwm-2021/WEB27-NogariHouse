18f17a45d6ba3fd51335cde4b1223df86c7c01de
정물결 <mulgyeoljung@gmail.com>
2021-11-08 17:01:34 +0900

    ✨ 로그인 API 요청 구현(Client)

--- client/src/views/signin-view.tsx
+++ client/src/views/signin-view.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
+// import { Redirect } from 'react-router-dom';
 
 import SignHeader from '@src/components/sign-header';
 import SignTitle from '@src/components/styled-components/sign-title';
@@ -29,16 +30,46 @@ const CustomInput = styled.input`
 `;
 
 function SignInView() {
+  const inputEmailRef = useRef<HTMLInputElement>(null);
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const inputOnChange = () => {
+    if (inputEmailRef.current?.value
+      && inputPasswordRef.current?.value) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+
+  const signIn = (): void => {
+    const loginInfo = {
+      email: inputEmailRef.current?.value,
+      password: inputPasswordRef.current?.value,
+    };
+
+    fetch(`${process.env.REACT_APP_API_URL}/api/user/signin`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(loginInfo),
+
+    }).then((res) => console.log(res.body))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <>
       <SignHeader />
       <SignBody>
         <SignTitle title="sign in with user email" />
         <CustomInputBox>
-          <CustomInput type="text" placeholder="E-mail Address" />
-          <CustomInput type="password" placeholder="Password" />
+          <CustomInput ref={inputEmailRef} onChange={inputOnChange} type="text" placeholder="E-mail Address" />
+          <CustomInput ref={inputPasswordRef} onChange={inputOnChange} type="password" placeholder="Password" />
         </CustomInputBox>
-        <DefaultButton buttonType="secondary" size="large">
+        <DefaultButton buttonType="secondary" size="large" onClick={() => { signIn(); }} isDisabled={isDisabled}>
           NEXT
         </DefaultButton>
       </SignBody>
