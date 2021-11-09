8211f3cb223e697654d6291ca33708b038087a72
J185_정물결 <mulgyeoljung@gmail.com>
2021-11-09 21:47:08 +0900

    ♻️ refreshToken 모델 간소화
    불필요한 필드 삭제

--- server/src/models/refresh-token.ts
+++ server/src/models/refresh-token.ts
@@ -4,33 +4,11 @@ import { Schema, Document, model } from 'mongoose';
 export interface IRefreshTokenTypesModel extends Document{
   userId: String,
   token: String,
-  expires: Date,
-  created: Date,
-  createdByIp: String,
-  revoked: Date,
-  revokedByIp: String,
-  replacedByToken: String,
-  isExpired?: boolean,
-  isActive?: boolean,
 }
 
-const schema = new Schema({
+const refreshTokenSchema = new Schema({
   userId: String,
   token: String,
-  expires: Date,
-  created: { type: Date, default: Date.now },
-  createdByIp: String,
-  revoked: Date,
-  revokedByIp: String,
-  replacedByToken: String,
 });
 
-schema.virtual('isExpired').get(function (this:IRefreshTokenTypesModel): boolean {
-  return Date.now() >= this.expires.getTime();
-});
-
-schema.virtual('isActive').get(function (this:IRefreshTokenTypesModel) {
-  return !this.revoked && !this.isExpired;
-});
-
-export default model<IRefreshTokenTypesModel>('RefreshToken', schema);
+export default model<IRefreshTokenTypesModel>('RefreshToken', refreshTokenSchema);
