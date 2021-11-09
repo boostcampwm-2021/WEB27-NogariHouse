1d236bbeeda54756111b697d77e0b824af9f94b6
ChanHui <kinm1596@daum.net>
2021-11-09 18:11:29 +0900

    🚧  로그인 이후 user 정보 전역상태에 저장해주는 코드 필요
    test 코드로 userDocumentId를 지정해주었습니다

--- client/src/recoil/atoms/user.ts
+++ client/src/recoil/atoms/user.ts
@@ -7,11 +7,13 @@ import { atom } from 'recoil';
 
 interface IUser {
   roomId: string,
+  userDocumentId: string,
 }
 
 export default atom<IUser>({
   key: 'user', // 해당 atom의 고유 key
   default: {
     roomId: '',
+    userDocumentId: '유저 다큐멘트 아이디 입니다.',
   },
 });
