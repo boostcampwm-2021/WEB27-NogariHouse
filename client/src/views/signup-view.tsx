83986d9c509cdd897267948ccec26f39d0250964
iHoHyeon <hyunee169@naver.com>
2021-11-09 14:26:48 +0900

    ✨ Client에서 인증번호 확인 구현

--- client/src/views/signup-view.tsx
+++ client/src/views/signup-view.tsx
@@ -3,18 +3,31 @@ import React, { useCallback, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
-import SignHeader from '@src/components/sign-header';
-import SignTitle from '@src/components/styled-components/sign-title';
-import SignBody from '@src/components/styled-components/sign-body';
-import DefaultButton from '@src/components/styled-components/default-button';
+import SignHeader from '@components/sign-header';
+import { BackgroundWrapper } from '@styled-components/modal';
+import LoadingSpinner from '@styled-components/loading-spinner';
+import SignTitle from '@styled-components/sign-title';
+import SignBody from '@styled-components/sign-body';
+import DefaultButton from '@styled-components/default-button';
 import { CustomInputBox, CustomInputBar } from '@styled-components/custom-inputbar';
+import { testEmailValidation } from '@utils/index';
+
+const CustomBackgroundWrapper = styled(BackgroundWrapper)`
+  opacity: 0.4;
+`;
 
 function SignUpView() {
   const inputEmailRef = useRef<HTMLInputElement>(null);
   const inputVerificationRef = useRef<HTMLInputElement>(null);
+  const verificationNumber = useRef<number>();
   const [isEmailInputView, setIsEmailInputView] = useState(true);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
+
+  const setVerificationNumber = useCallback((number: number) => {
+    verificationNumber.current = number;
+  }, []);
 
   const inputOnChange = useCallback(() => {
     if (inputEmailRef.current?.value
@@ -26,16 +39,46 @@ function SignUpView() {
   }, []);
 
   const onClickEmailNextButton = () => {
-    setIsDisabled(true);
-    setIsEmailInputView(false);
+    const inputEmailValue = inputEmailRef.current?.value as string;
+
+    if (testEmailValidation(inputEmailValue)) {
+      setLoading(true);
+
+      const postSignupMailConfig = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: inputEmailValue }),
+      };
+
+      fetch(`${process.env.REACT_APP_API_URL}/api/user/signup/mail`, postSignupMailConfig)
+        .then((res) => res.json())
+        .then((json) => json.verificationNumber)
+        .then(setVerificationNumber)
+        .then(() => {
+          setLoading(false);
+          setIsDisabled(true);
+          setIsEmailInputView(false);
+        });
+    } else {
+      alert('올바른 이메일을 입력해주세요');
+    }
   };
 
   const onClickVerificationNextButton = () => {
-    history.replace('/signup/info');
+    const inputVerificationValue = inputVerificationRef.current?.value as string;
+
+    if (inputVerificationValue === (verificationNumber.current?.toString())) {
+      history.replace('/signup/info');
+    } else {
+      alert('인증번호를 확인하세요.');
+    }
   };
 
   return (
     <>
+      {loading && <CustomBackgroundWrapper><LoadingSpinner /></CustomBackgroundWrapper>}
       <SignHeader />
       <SignBody>
         {isEmailInputView
