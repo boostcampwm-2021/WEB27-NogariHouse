d12823919f1776dc8f96d00dc57e237219b726e3
iHoHyeon <hyunee169@naver.com>
2021-11-12 02:09:32 +0900

    ♻️ API수정 keyword query -> params

--- server/src/api/routes/search.ts
+++ server/src/api/routes/search.ts
@@ -7,12 +7,13 @@ const searchRouter = Router();
 export default (app: Router) => {
   app.use('/search', searchRouter);
 
-  searchRouter.get('/events', async (req: Request, res: Response) => {
-    const { keyword, count } = req.query;
+  searchRouter.get('/events/:keyword', async (req: Request, res: Response) => {
+    const { keyword } = req.params;
+    const { count } = req.query;
     if (typeof keyword !== 'string' || typeof count !== 'string') {
         res.json({ ok: false });
     } else {
-        const items = await eventsService.searchEvent(keyword, Number(count));
+        const items = (await eventsService.searchEvent(keyword, Number(count)))?.map(eventsService.makeItemToEventInterface);
         res.json({ ok: true, items, keyword })
     }
   });
