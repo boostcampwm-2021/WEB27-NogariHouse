d52f0ca55d1a7c873a2ded1aa29be95abecfe882
Mulgyeol <mulgyeoljung@gmail.com>
2021-11-15 18:22:14 +0900

    ✅ UserCardList 테스트를 위한 following-view 작성
    userCard와 userCardList의 컴포넌트 사용을 확인 가능

--- client/src/views/following-view.tsx
+++ client/src/views/following-view.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import UserCard from '@common/user-card';
+import UserCardList from '@src/components/common/user-card-list';
 
 function FollowingView() {
   const testUser1 = {
+    key: '1',
     userName: 'Mulgyeol1',
     userDesc: 'test for1',
     profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/default-user-image.png',
@@ -11,6 +13,7 @@ function FollowingView() {
   };
 
   const testUser2 = {
+    key: '2',
     userName: 'Mulgyeol2',
     userDesc: 'test for2',
     profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/default-user-image.png',
@@ -18,19 +21,28 @@ function FollowingView() {
   };
 
   const testUser3 = {
+    key: '3',
     userName: 'Mulgyeol1',
     userDesc: 'test for1',
     profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/default-user-image.png',
   };
 
   const testUser4 = {
+    key: '4',
     userName: 'Mulgyeol2',
     userDesc: 'test for2',
     profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/default-user-image.png',
   };
 
+  const userList1 = [testUser1, testUser2];
+  const userList2 = [testUser3, testUser4];
+
+  const clickEvent = () => (alert('click'));
+
   return (
     <>
+      <UserCardList userList={userList1} cardType="follow" />
+      <UserCardList userList={userList2} cardType="others" clickEvent={clickEvent} />
       <UserCard userData={testUser1} cardType="follow" />
       <UserCard userData={testUser2} cardType="follow" />
       <UserCard userData={testUser3} cardType="others" />
