feae3b734dc3790ca45d1c7446ce6cb591f60942
ChanHui <kinm1596@daum.net>
2021-11-15 15:35:40 +0900

    ✨  채팅방 기본 레이아웃 구현

--- client/src/views/chat-rooms-view.tsx
+++ client/src/views/chat-rooms-view.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import ChatHeader from '@components/chat/chat-header';
+
+const ChatRoomsLayout = styled.div`
+  background-color: #FFFFFF;
+  border-radius: 30px;
+
+  width: 100%;
+  height: 100%;
+
+  overflow: hidden;
+  position: relative;
+`;
 
 function ChatRoomsViews() {
-  return (<></>);
+  return (
+    <ChatRoomsLayout>
+      <ChatHeader />
+    </ChatRoomsLayout>
+  );
 }
 
 export default ChatRoomsViews;
