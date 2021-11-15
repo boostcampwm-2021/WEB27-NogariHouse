2d767ecad72ebb59dfa58446efa3012a847ec03c
ChanHui <kinm1596@daum.net>
2021-11-15 16:37:40 +0900

    ✨  chat-user-card 컴포넌트 구현
    API를 아직 구현안한 관계로 더미데이터 추가

--- client/src/views/chat-rooms-view.tsx
+++ client/src/views/chat-rooms-view.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ChatHeader from '@components/chat/chat-header';
+import ChatUserCard from '@components/chat/chat-user-card';
 
 const ChatRoomsLayout = styled.div`
   background-color: #FFFFFF;
@@ -14,10 +15,21 @@ const ChatRoomsLayout = styled.div`
   position: relative;
 `;
 
+const dummyData = [
+  { _id: 'hello', userName: 'helo', profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/cat.jpeg' },
+  { _id: 'hello', userName: 'test!', profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/puppy2.jpeg' },
+  { _id: 'hello', userName: 'ChanHUi', profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/puppy2.jpeg' },
+];
+
+// const dummyData = [
+//   { _id: 'hello', userName: 'helo', profileUrl: 'https://kr.object.ncloudstorage.com/nogarihouse/profile/cat.jpeg' },
+// ];
+
 function ChatRoomsViews() {
   return (
     <ChatRoomsLayout>
       <ChatHeader />
+      <ChatUserCard participantsInfo={dummyData} />
     </ChatRoomsLayout>
   );
 }
