d551ed4e50735d839f68a4909605ca3683d85fd4
Mulgyeol <mulgyeoljung@gmail.com>
2021-11-18 17:16:40 +0900

    ✨ following 페이지에서 로그인중인 유저가 following 중인지 필터링하는 기능 구현

--- client/src/views/following-view.tsx
+++ client/src/views/following-view.tsx
@@ -1,34 +1,33 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
-// import { useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 
 import LoadingSpinner from '@common/loading-spinner';
 import UserCardList from '@src/components/common/user-card-list';
 import useFetchItems from '@src/hooks/useFetchItems';
-// import followingListState from '@atoms/following-list';
+import followingListState from '@atoms/following-list';
 
 function FollowingView({ match }:any) {
   const userId = match.params.id;
   const [nowItemList, nowItemType] = useFetchItems<any>(`/user/followings/${userId}`, 'followings');
   const [loading, setLoading] = useState(true);
-  // const [userFollowingList, setUserFollowingList] = useState([]);
-  // const myFollowingList = useRecoilValue(followingListState);
+  const myFollowingList = useRecoilValue(followingListState);
 
-  // const makeUserObjectIncludedIsFollow = (
-  //   userItem: {
-  //     _id: string,
-  //     userName:
-  //     string,
-  //     description: string,
-  //     profileUrl: string
-  //   },
-  // ) => ({
-  //   _id: userItem._id,
-  //   userName: userItem.userName,
-  //   description: userItem.description,
-  //   profileUrl: userItem.profileUrl,
-  //   isFollow: !!myFollowingList.includes(userItem._id),
-  // });
+  const makeUserObjectIncludedIsFollow = (
+    userItem: {
+      _id: string,
+      userName:
+      string,
+      description: string,
+      profileUrl: string
+    },
+  ) => ({
+    _id: userItem._id,
+    userName: userItem.userName,
+    description: userItem.description,
+    profileUrl: userItem.profileUrl,
+    isFollow: !!myFollowingList.includes(userItem._id),
+  });
 
   useEffect(() => {
     if (nowItemList && nowItemType === 'followings') {
@@ -37,18 +36,13 @@ function FollowingView({ match }:any) {
     }
   });
 
-  // useEffect(() => {
-  //   const followingListIncludedIsFollow = nowItemList.map(makeUserObjectIncludedIsFollow);
-  //   setUserFollowingList(followingListIncludedIsFollow);
-  // }, [nowItemList]);
-
   if (loading) {
     return <LoadingSpinner />;
   }
 
   return (
     <>
-      <UserCardList userList={nowItemList} cardType="follow" />
+      <UserCardList userList={nowItemList.map(makeUserObjectIncludedIsFollow)} cardType="follow" />
     </>
   );
 }
