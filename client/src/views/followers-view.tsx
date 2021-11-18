4850b381343dd76f7718542a74035acbbd6ce2ec
Mulgyeol <mulgyeoljung@gmail.com>
2021-11-18 17:43:27 +0900

    ✨ 다른사람의 팔로잉, 팔로우 목록에서 자기 자신의 버튼은 안나오도록 필터링(수정 필요)
    카드의 크기가 others로 좀 작다.

--- client/src/views/followers-view.tsx
+++ client/src/views/followers-view.tsx
@@ -3,15 +3,17 @@ import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import LoadingSpinner from '@common/loading-spinner';
-import UserCardList from '@src/components/common/user-card-list';
 import useFetchItems from '@src/hooks/useFetchItems';
 import followingListState from '@atoms/following-list';
+import UserCard from '@common/user-card';
+import userState from '@atoms/user';
 
 function FollowerView({ match }:any) {
   const userId = match.params.id;
   const [nowItemList, nowItemType] = useFetchItems<any>(`/user/followers/${userId}`, 'followers');
   const [loading, setLoading] = useState(true);
   const myFollowingList = useRecoilValue(followingListState);
+  const user = useRecoilValue(userState);
 
   const makeUserObjectIncludedIsFollow = (
     userItem: {
@@ -29,6 +31,29 @@ function FollowerView({ match }:any) {
     isFollow: !!myFollowingList.includes(userItem._id),
   });
 
+  const makeItemToCardForm = (item: any) => {
+    const newUserItemForm = makeUserObjectIncludedIsFollow(item);
+    if (newUserItemForm._id === user.userDocumentId) {
+      return (
+        <UserCard
+      // eslint-disable-next-line no-underscore-dangle
+          key={newUserItemForm._id}
+          cardType="others"
+          userData={newUserItemForm}
+        />
+      );
+    }
+
+    return (
+      <UserCard
+          // eslint-disable-next-line no-underscore-dangle
+        key={newUserItemForm._id}
+        cardType="follow"
+        userData={newUserItemForm}
+      />
+    );
+  };
+
   useEffect(() => {
     if (nowItemList && nowItemType === 'followers') {
       console.log(nowItemList);
@@ -42,7 +67,7 @@ function FollowerView({ match }:any) {
 
   return (
     <>
-      <UserCardList userList={nowItemList.map(makeUserObjectIncludedIsFollow)} cardType="follow" />
+      {nowItemList.map(makeItemToCardForm)}
     </>
   );
 }
