d6c2fc6d96f12fc2629f24aeb0841fa52651f210
ChanHui <kinm1596@daum.net>
2021-11-24 00:00:06 +0900

    💄  채팅방 만들기 페이지 헤더 반응형으로 개선

--- client/src/components/chat/chat-new-header.tsx
+++ client/src/components/chat/chat-new-header.tsx
@@ -1,39 +1,53 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-len */
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { useHistory } from 'react-router-dom';
 
 import useChatSocket from '@utils/chat-socket';
-import { ChatHeaderStyle } from '@components/chat/style';
 import { postChatRoom } from '@api/index';
 import selectedUserType from '@atoms/chat-selected-users';
 import userType from '@atoms/user';
 
-const BtnStyle = css`
-  position: absolute;
-  transform: translateY(30px);
+const ChatNewHeaderWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  background-color: #B6B6B6;
+`;
 
-  font-size: 20px;
+const ChatNewHeader = styled.div`
+  width: 90%;
+  height: 80px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+
+  p{
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: auto;
+
+    font-weight: Bold;
+    font-size: min(4vw,28px);
+
+    margin: 0px;
+  }
+`;
+
+const BtnStyle = styled.button`
+  font-size: min(4vw,28px);
 
   background-color: transparent;
   border: none;
+  color: #58964F;
 
   &:hover {
     filter: invert(88%) sepia(1%) saturate(4121%) hue-rotate(12deg) brightness(62%) contrast(79%);
   }
-`;
-
-const CanCelBtnStyle = styled.button`
-  left: 5%;
-  color: #58964F;
-  ${BtnStyle};
-`;
-
-const DoneBtnStyle = styled.button`
-  right: 5%;
-  color: #58964F;
-  ${BtnStyle};
 `;
 
 const DoneBtn = () => {
@@ -59,7 +73,7 @@ const DoneBtn = () => {
   };
 
   return (
-    <DoneBtnStyle onClick={makeChatRoom}>Done</DoneBtnStyle>
+    <BtnStyle onClick={makeChatRoom}>Done</BtnStyle>
   );
 };
 
@@ -72,15 +86,18 @@ const CancelBtn = () => {
     history.push({ pathname: '/chat-rooms' });
   };
 
-  return (<CanCelBtnStyle onClick={cancelEvent}>Cancel</CanCelBtnStyle>);
+  return (<BtnStyle onClick={cancelEvent}>Cancel</BtnStyle>);
 };
 
 export default function NewChatRoomHeader() {
   return (
-    <ChatHeaderStyle>
-      <CancelBtn />
-      <p>NEW MESSAGE</p>
-      <DoneBtn />
-    </ChatHeaderStyle>
+    <ChatNewHeaderWrap>
+      <ChatNewHeader>
+        <CancelBtn />
+        <p>NEW MESSAGE</p>
+        <DoneBtn />
+      </ChatNewHeader>
+    </ChatNewHeaderWrap>
+
   );
 }
