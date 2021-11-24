eec3c29a94bfd5de8d5826c97aa88dcd70ca3dd3
iHoHyeon <hyunee169@naver.com>
2021-11-24 16:07:41 +0900

    ✨ /activity GET 라우터 구현

--- server/src/api/routes/activity.ts
+++ server/src/api/routes/activity.ts
@@ -14,8 +14,9 @@ export default (app: Router) => {
   activityRouter.get('/', async (req: Request, res: Response) => {
     const { userDocumentId } = req.body;
     try {
-      const { activity } = await usersService.findUserByDocumentId(userDocumentId);
-      const items = await usersService.makeItemToActivityInterface(activity);
+      const user = await usersService.findUserByDocumentId(userDocumentId);
+      const activityList = user!.activity;
+      const items = await usersService.makeItemToActivityInterface(activityList);
       res.json({ ok: true, items });
     } catch (e) {
       console.error(e);
