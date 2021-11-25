407442d912602b18d234d5fef339d7e70bf35621
NEM-NE <dlatqdlatq@mju.ac.kr>
2021-11-25 22:15:51 +0900

    ♻️ chat-rooms-new-view에 useSelectUser적용

--- client/src/views/chat-rooms-new-view.tsx
+++ client/src/views/chat-rooms-new-view.tsx
@@ -1,62 +1,17 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useState, useRef } from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useRef } from 'react';
 
 import NewChatRoomHeader from '@src/components/chat/chat-new-header';
 import { ChatRoomsLayout, NewChatRoomBody } from '@components/chat/style';
 import UserCardList from '@components/common/user-card-list';
-import { findUsersByIdList } from '@api/index';
-import followState from '@atoms/following-list';
 import {
   SelectDiv, SelectInputBar, SelectedUserDiv, SelectUserComponent,
 } from '@common/select';
+import useSelectUser from '@hooks/useSelectUser';
 
 function ChatRoomsNewView() {
-  const followingList = useRecoilValue(followState);
-  const [selectedUsers, setSelectedUsers] = useState<any>([]);
-  const [allUserList, setAllUserList] = useState([]);
-  const [filteredUserList, setFilteredUserList] = useState([]);
   const inputBarRef = useRef<HTMLInputElement>(null);
-
-  const addSelectedUser = (e: any) => {
-    const userCardDiv = e.target.closest('.userCard');
-    const profileUrl = userCardDiv.querySelector('img');
-    if (!userCardDiv || !profileUrl) return;
-    const userName = userCardDiv?.getAttribute('data-username');
-    inputBarRef.current!.value = '';
-    setSelectedUsers([...selectedUsers,
-      {
-        userDocumentId: userCardDiv?.getAttribute('data-id'),
-        userName,
-        profileUrl: profileUrl.getAttribute('src'),
-      }]);
-  };
-
-  const deleteUser = (e: any) => {
-    setSelectedUsers(selectedUsers.filter((user: any) => user.userDocumentId !== e.target.getAttribute('data-id')));
-    inputBarRef.current!.value = '';
-  };
-
-  const searchUser = () => {
-    const searchWord = inputBarRef.current!.value;
-    const selectedUserIds = selectedUsers.map((user: any) => user.userDocumentId);
-    setFilteredUserList(allUserList
-      .filter((user: any) => (user.userId.indexOf(searchWord) > -1 || user.userName.indexOf(searchWord) > -1)
-      && selectedUserIds.indexOf(user._id) === -1));
-  };
-
-  useEffect(() => {
-    findUsersByIdList(followingList).then((res: any) => {
-      const selectedUserIds = selectedUsers.map((user: any) => user.userDocumentId);
-      setAllUserList(res.userList);
-      setFilteredUserList(res.userList.filter((user: any) => selectedUserIds.indexOf(user._id) === -1));
-    });
-  }, [followingList]);
-
-  useEffect(() => {
-    const selectedUserIds = selectedUsers.map((user: any) => user.userDocumentId);
-    setFilteredUserList(allUserList.filter((user: any) => selectedUserIds.indexOf(user._id) === -1));
-  }, [selectedUsers]);
+  const [selectedUsers, filteredUserList, searchUser, deleteUser, addSelectedUser] = useSelectUser(inputBarRef);
 
   return (
     <ChatRoomsLayout>
