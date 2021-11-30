55148a4742bbee1ec8809ac28b2f9ec06aa18c4c
Mulgyeol <mulgyeoljung@gmail.com>
2021-11-30 18:52:15 +0900

    🐛 아이폰 브라우저 상 하단 고려 레이아웃

--- client/src/components/main/right-sidebar/index.tsx
+++ client/src/components/main/right-sidebar/index.tsx
@@ -18,7 +18,6 @@ const RoomModalLayout = styled.div`
   width: 100%;
   height: 100%;
   min-width: 320px;
-  margin-bottom: 10%;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   position: relative;
 
