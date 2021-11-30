64144ef3e3bab683244318722fb42cb06b834504
iHoHyeon <hyunee169@naver.com>
2021-11-30 19:38:35 +0900

    ♻️ socket message 상수화 - room

--- client/src/constants/socket-message/room.ts
+++ client/src/constants/socket-message/room.ts
@@ -1,1 +1,17 @@
-export default {};
+type TRoomSocketMessage = {
+    join: string,
+    offer: string,
+    answer: string,
+    ice: string,
+    mic: string,
+    leave: string
+}
+
+export default {
+  join: 'room:join',
+  offer: 'room:offer',
+  answer: 'room:answer',
+  ice: 'room:ice',
+  mic: 'room:mic',
+  leave: 'room:leave',
+} as TRoomSocketMessage;
